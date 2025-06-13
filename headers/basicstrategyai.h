14c822c4a183946c4014cee5bd319602fb42a566
Vlsr1 <vladsmirnov300602@gmail.com>
2025-06-14 01:06:52 +0300

    Added basic bot and ability to choose it

---
+++ headers/basicstrategyai.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class BasicStrategyAI {
+public:
+    enum class Action {
+        HIT,
+        STAND,
+        DOUBLE,
+        SPLIT,
+        SURRENDER
+    };
+
+    BasicStrategyAI();
+
+    // Main decision making function
+    Action getAction(int playerScore, int dealerUpCard, bool isSoftHand, bool canSplit) const;
+
+    // Helper functions
+    bool isSoftHand(const std::vector<int>& cards) const;
+    bool canDoubleDown(int playerScore) const;
+    bool shouldTakeInsurance() const;
+
+private:
+    // Strategy helper functions
+    Action getHardHandAction(int playerScore, int dealerUpCard) const;
+    Action getSoftHandAction(int playerScore, int dealerUpCard) const;
+    Action getPairAction(int pairValue, int dealerUpCard) const;
+    
+    // Utility functions
+    bool isDealerCardWeak(int dealerCard) const;
+    bool isDealerCardStrong(int dealerCard) const;
+    bool isDealerCardVeryWeak(int dealerCard) const;
+    bool isDealerCardVeryStrong(int dealerCard) const;
+}; 
